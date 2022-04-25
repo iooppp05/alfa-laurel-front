0ed5ef5ee1914ec305473fb5b79f2faeebc18d14
Sagit Gutierrez <jozaguts@gmail.com>
2022-04-25 09:55:36 -0500

    snackbar agregada

--- src/store/settings.js
+++ src/store/settings.js
@@ -3,6 +3,13 @@ export default {
   state: {
     drawerState: false,
     permissions: null,
+    loading: false,
+    snackbar: {
+      value: false,
+      text: "",
+      color: "warning",
+      bottom: true,
+    },
     links: [
       { text: "Materias", icon: "mdi-bookshelf" },
       { text: "Exámenes", icon: "mdi-file-multiple-outline" },
@@ -11,6 +18,18 @@ export default {
   mutations: {
     TOGGLE_DRAWER(state, status) {
       state.drawerState = status;
+    },
+    TOGGLE_LOADING(state, loading) {
+      state.loading = loading;
+    },
+    SHOW_SNACKBAR(state, { value, text, color = "warning", bottom = true }) {
+      state.snackbar = {
+        value,
+        text,
+        color,
+        bottom,
+      };
+      setTimeout(() => (state.snackbar.value = false), 3000);
     },
   },
   getters: {
