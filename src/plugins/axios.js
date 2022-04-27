3ac923558f10920732151eb03809165ba770b140
Sagit Gutierrez <jozaguts@gmail.com>
2022-04-27 18:29:45 -0500

    production route

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://office.alfalaurel.com/";
 axios.interceptors.request.use(
   (config) => {
     const token = window.localStorage.getItem("token");
