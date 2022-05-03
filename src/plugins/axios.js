9a0b8675a72f41b68c1f8b94f645bdb9ca8588d6
Sagit Gutierrez <jozaguts@gmail.com>
2022-05-03 00:32:50 -0500

    commons headers added by axios & doble check null token

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
+axios.defaults.headers.common["Accept"] = "application/json";
+axios.defaults.headers.common["Content-Type"] = "application/x-www-form-urlencoded ";
 axios.defaults.baseURL =
   process.env.NODE_ENV === "production"
     ? "https://office.alfalaurel.com"
-    : "http://localhost:8000";
+    : "http://127.0.0.1:8000";
 axios.interceptors.request.use(
   (config) => {
     const token = window.localStorage.getItem("token");
-    config.headers.Accept = "application/json";
-    // config.headers.ContentType = "application/json";
-    if (token != null) {
+    if (token !== null && token !== "null") {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
