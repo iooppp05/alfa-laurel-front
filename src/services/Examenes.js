b95fb474d23742cda8666266d26f677c97983692
Sagit Gutierrez <jozaguts@gmail.com>
2022-05-03 15:35:39 -0500

    export services by functions

--- src/services/Examenes.js
+++ src/services/Examenes.js
@@ -1,4 +1,9 @@
 import axios from "axios";
+import { Users } from "@/services/Users";
+import { Subjects } from "@/services/Subjects";
+const UserService = new Users();
+const SubjectsService = new Subjects();
+
 export class Examenes {
   async store(data) {
     let fd = new FormData();
@@ -23,4 +28,35 @@ export class Examenes {
   async delete({ examenId }) {
     return await axios.delete(`/api/examenes/${examenId}`);
   }
+}
+export async function all() {
+  return await axios.get("/api/examenes");
+}
+export async function store(data) {
+  let fd = new FormData();
+  if (data.file) {
+    fd.append("file", data.file);
+    fd.append("user_id", data.user_id);
+    fd.append("subject_id", data.subject_id);
+    fd.append("name", data.name);
+  }
+  return await axios.post("/api/examenes", data.file ? fd : data, {
+    headers: {
+      "Content-Type": data.file ? "multipart/form-data" : "application/json",
+    },
+  });
+}
+export async function initExamenes() {
+  let { data: examenes } = await all();
+  let { data: users } = await UserService.all();
+  let { data: subjects } = await SubjectsService.all();
+  return {
+    subjects: subjects.map((item) => {
+      return { value: item.id, text: item.name };
+    }),
+    users: users.map((item) => {
+      return { value: item.id, text: item.name };
+    }),
+    examenes,
+  };
 }
