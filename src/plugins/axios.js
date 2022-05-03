61fc9baada23c2170a8a29d1a38a8127c34405a5
Sagit Gutierrez <jozaguts@gmail.com>
2022-05-03 15:32:10 -0500

    Access-Control-Allow header added

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,15 +1,17 @@
 import axios from "axios";
-axios.defaults.withCredentials = true;
 axios.defaults.headers.common["Accept"] = "application/json";
-axios.defaults.headers.common["Content-Type"] = "application/x-www-form-urlencoded ";
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "true";
+axios.defaults.headers.common["Content-Type"] =
+  "application/x-www-form-urlencoded ";
 axios.defaults.baseURL =
   process.env.NODE_ENV === "production"
     ? "https://office.alfalaurel.com"
-    : "http://127.0.0.1:8000";
+    : "http://localhost:8000";
+axios.defaults.withCredentials = true;
 axios.interceptors.request.use(
   (config) => {
-    const token = window.localStorage.getItem("token");
-    if (token !== null && token !== "null") {
+      const token = window.localStorage.getItem("token");
+    if (token !== null) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
