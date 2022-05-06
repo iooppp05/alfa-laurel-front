30edbc2006a265933396ac8537c5c25e29fbe239
Sagit Gutierrez <jozaguts@gmail.com>
2022-05-05 23:07:32 -0500

    model moved to store

--- src/components/Exams/FileForm.vue
+++ src/components/Exams/FileForm.vue
@@ -13,20 +13,20 @@
         <v-col cols="12">
           <v-text-field
             label="Titulo del examen"
-            v-model="editedItem.name"
+            v-model="name"
           ></v-text-field>
         </v-col>
         <v-col cols="6">
           <v-select
             label="Materia"
             :items="subjects"
-            v-model="editedItem.subject_id"
+            v-model="subject_id"
           >
           </v-select>
         </v-col>
         <v-col cols="6">
           <v-select
-            v-model="editedItem.user_id"
+            v-model="user_id"
             label="Profesor"
             :items="users"
           >
@@ -36,7 +36,7 @@
           <v-file-input
             show-size
             accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-            v-model="editedItem.file"
+            v-model="file"
           ></v-file-input>
         </v-col>
         <v-col cols="12">
@@ -55,22 +55,71 @@
 </template>
 
 <script>
+import { store } from "@/services/Examenes";
 export default {
   name: "FileForm",
-  props: ["subjects", "users", "loading"],
-  methods: {
-    async save() {
-      await this.$emit('create-exam-by-file', this.editedItem)
-      await this.$refs.form.reset()
-      // this.editedItem = { name: null, user_id: null, subject_id: null, file: null }
-
+  props: ["subjects", "users",],
+  data() {
+    return  {
+      loading: false,
     }
   },
-  data() {
-    return {
-      //todo quitar esto de aqui moverlo al sot
-      editedItem: { name: null, user_id: null, subject_id: null, file: null },
-    };
+  methods: {
+    async save() {
+      try {
+        this.loading = true;
+        await store(this.$store.state.examen.editedItem);
+        this.$emit("saved")
+
+        //todo manejar el guardado por el index
+        // await this.showSnackBar(
+        //     "Examen agregado correctamente",
+        //     "success"
+        // );
+      } catch (e) {
+        this.showSnackBar( e.message);
+      } finally {
+        this.$store.commit("examen/CLOSE_UPDATE_DIALOG")
+        this.$store.commit("examen/RESET_FORM")
+        this.$store.commit("examen/SET_STEP",1)
+        await this.$refs.form.reset()
+        this.loading = false;
+      }
+    },
+  },
+  computed:{
+    name:{
+      get(){
+        return this.$store.state.examen.editedItem.name ;
+      },
+      set(value){
+        this.$store.commit("examen/UPDATE_NAME",value);
+      }
+    },
+    user_id:{
+      get(){
+        return this.$store.state.examen.editedItem.user_id;
+      },
+      set(value){
+        this.$store.commit("examen/UPDATE_USER_ID", value);
+      }
+    },
+    subject_id:{
+      get(){
+        return this.$store.state.examen.editedItem.subject_id ;
+      },
+      set(value){
+        this.$store.commit("examen/UPDATE_SUBJECT_ID", value);
+      }
+    },
+    file:{
+      get(){
+        return this.$store.state.examen.editedItem.file ;
+      },
+      set(value){
+        this.$store.commit("examen/UPDATE_FILE", value);
+      }
+    },
   },
 };
 </script>
