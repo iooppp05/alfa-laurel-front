b6727151283bb8040b06abfa2f76517c1d53aa8b
Sagit Gutierrez <jozaguts@gmail.com>
2022-05-06 23:22:25 -0500

    update button load data and shows dialog

--- src/components/Exams/UpdateButton.vue
+++ src/components/Exams/UpdateButton.vue
@@ -20,22 +20,42 @@ export default {
     show(id) {
       this.loading = !this.loading;
       ExamenesService.get(id)
-        .then(({ data }) => {
-          this.$store.commit("examen/UPDATE_NAME", data.data.name);
-          this.$store.commit("examen/UPDATE_SUBJECT_ID", data.data.subject_id);
-          this.$store.commit("examen/UPDATE_USER_ID", data.data.user_id);
-          return { data };
-        })
-        .then(({ data }) => {
-          this.$store.commit(
-            "examen/SET_EXAMEN_QUESTIONS",
-            data.data.questions
-          );
-        })
-        .then(() => {
-          this.loading = !this.loading;
-        })
-        .then(() => this.$store.commit("examen/OPEN_UPDATE_DIALOG"));
+          .then(({ data }) => {
+            data.data.questions.map(question => {
+              question.options.map((option,index) => {
+                if (index === 0 || index === 1){
+                  option.cols = "12";
+                  option.md = "4";
+                } else{
+                  option.cols = "10";
+                  option.md = "3";
+                }
+              })
+            })
+            this.$store.commit(
+                "examen/SET_EXAMEN_QUESTIONS",
+                data.data.questions
+            );
+            return data
+          })
+          .then(({ data }) => {
+            this.$nextTick(()=>{
+              this.$store.commit("examen/UPDATE_NAME", data.name);
+              this.$store.commit("examen/UPDATE_EXAMEN_ID", data.examen_id);
+              this.$store.commit("examen/UPDATE_SUBJECT_ID", data.subject_id);
+              this.$store.commit("examen/UPDATE_USER_ID", data.user_id);
+            })
+          })
+          .then(() => {
+            this.$nextTick(()=>{
+              this.loading = !this.loading;
+            })
+          })
+          .then(() => {
+            this.$nextTick(()=>{
+              this.$store.commit("examen/OPEN_UPDATE_DIALOG")
+            })
+          });
     },
   },
 };
