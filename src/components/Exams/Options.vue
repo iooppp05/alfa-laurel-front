a92893d5fcd414be2817851a5325dba31520e6eb
Sagit Gutierrez <jozaguts@gmail.com>
2022-05-06 23:20:12 -0500

    dialog remove question added

--- src/components/Exams/Options.vue
+++ src/components/Exams/Options.vue
@@ -12,22 +12,48 @@
       ></v-text-field>
     </v-col>
     <v-col cols="1" md="1" class="align-center d-flex">
-      <v-tooltip right>
-        <template #activator="{ on }">
-          <v-btn @click="$emit('remove-question')" color="error" tile small v-on="on">
-            <v-icon>mdi-delete</v-icon>
-          </v-btn>
-        </template>
-        <span>Eliminar pregunta</span>
-      </v-tooltip>
+      <v-btn
+          color="error"
+          tile
+          small
+          @click="dialog = true"
+      >
+        <v-icon>mdi-delete</v-icon>
+      </v-btn>
     </v-col>
+    <v-dialog :value="dialog" max-width="454px">
+      <v-card >
+        <v-card-title class="title grey--text text--darken-1">Se eliminara la pregunta  ¿Estas de acuerdo?</v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" outlined text @click="dialog = false"
+          >Cancelar</v-btn
+          >
+          <v-btn
+              color="primary"
+              @click="removeQuestion"
+              :loading="loading"
+          >Aceptar</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+<!--    @click="removeRow"-->
   </v-row>
+
 </template>
 
 <script>
+import { deleteExamQuestion } from "@/services/Examenes"
 export default {
   name: "Options",
-  props: ["options"],
+  props: ["options", "questionID"],
+  data() {
+    return {
+      dialog: false,
+      loading: false,
+    }
+  },
   watch: {
     options: {
       deep: true,
@@ -36,5 +62,28 @@ export default {
       },
     },
   },
+  methods:{
+     async removeQuestion() {
+      this.loading = true;
+      try{
+        if (!this.$store.state.examen.dialog) {
+         let {data} = await deleteExamQuestion({examenID: this.$store.state.examen.editedItem.examen_id, questionID:this.questionID})
+          if (data.success) {
+            this.$store.commit("settings/SHOW_SNACKBAR",{text: "Pregunta eliminada correctamente",color: "success"});
+          }else if (!data.success){
+            this.$store.commit("settings/SHOW_SNACKBAR",{text: "No fue posible eliminar la pregunta"});
+          }
+        }
+      }catch (e) {
+        console.log(e.message)
+      }finally {
+        this.$nextTick(()=>{
+          this.$store.commit("examen/REMOVE_QUESTION", this.questionID)
+          this.loading = false;
+          this.dialog = false;
+        })
+      }
+    }
+  }
 };
 </script>
